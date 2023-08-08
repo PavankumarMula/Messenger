d030d89aebcd6af217cc33fd35796aab735431cc
PavankumarMula <pavanpappy7@gmail.com>
2023-08-08 17:52:44 +0530

    Sign Up Page Is Added in client side

--- client/src/Components/Signup.js
+++ client/src/Components/Signup.js
@@ -5,26 +5,51 @@ const Signup = () => {
   const [name, setName] = useState("");
   const [mail, setMail] = useState("");
   const [password, setPassword] = useState("");
+  const [phone, setPhone] = useState("");
 
   // form handler
   const formHandler = (e) => {
     e.preventDefault();
-    console.log(name, mail, password);
+    console.log(name, mail, password, phone);
+    setName("");
+    setMail("");
+    setPassword("");
+    setPhone("");
   };
 
   return (
     <>
       <div className={styles.container}>
         <form onSubmit={formHandler} className={styles.formCard}>
+
           <label>Name</label>
-          <input type="text" onChange={(e) => setName(e.target.value)}></input>
+          <input
+            type="text"
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+          ></input>
+
           <label>Email</label>
-          <input type="email" onChange={(e) => setMail(e.target.value)}></input>
+          <input
+            type="email"
+            onChange={(e) => setMail(e.target.value)}
+            value={mail}
+          ></input>
+
+          <label>Phone</label>
+          <input
+            type="phone"
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}
+          ></input>
+
           <label>Password</label>
           <input
             type="password"
             onChange={(e) => setPassword(e.target.value)}
+            value={password}
           ></input>
+          
           <button>Submit</button>
           <button>Have An Account?</button>
         </form>
